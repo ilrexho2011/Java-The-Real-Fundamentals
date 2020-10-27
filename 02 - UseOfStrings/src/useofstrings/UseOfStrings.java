264e799773bcdd217c53614d2d23d16d5a249bc8
Ilirjan Rexho <61479363+ilrexho2011@users.noreply.github.com>
2020-10-27 19:29:28 +0100

    Add files via upload

---
+++ 02 - UseOfStrings/src/useofstrings/UseOfStrings.java
@@ -0,0 +1,33 @@
+/*
+ * An aplication to illustrate os of variables especially Strings.
+ */
+package useofstrings;
+
+/**
+ * @author IR
+ */
+public class UseOfStrings {
+
+    public static void main(String[] args) {
+        int vol = 3;
+        char c = '*';
+        String album = "Welcome back";
+        String genre = "Rock";
+        String author = "Sea boys";
+        String year = "2020";
+        System.out.print(c);
+        System.out.print(c);
+        System.out.print(c);
+        System.out.print(c);
+        System.out.print(c);
+        System.out.print(c);
+        System.out.print(c);
+        System.out.println(c);
+        System.out.println("Album: " + album);
+        System.out.println("Genre: " + genre);
+        System.out.println("Author: " + author);
+        System.out.println("Year: " + year);
+        System.out.println("This was " + vol + "'d album for \"Welcome back\" - Rock band");
+    }
+    
+}
